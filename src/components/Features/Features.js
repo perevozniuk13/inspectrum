959c5cccf7b251658d49b88642ad51227f102f65
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-13 17:30:48 +0100

    create homepage features structure, add basic styling

---
+++ src/components/Features/Features.js
@@ -0,0 +1,53 @@
+import "./Features.scss";
+
+export default function Features() {
+  return (
+    <>
+      <section className="features">
+        <h2 className="features__title">Features</h2>
+
+        <article className="features__container">
+          <div className="feature">
+            <h3 className="feature__title">Feature title</h3>
+            <p className="feature__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit illum alias et aliquid consequuntur provident nulla.
+            </p>
+          </div>
+
+          <div className="feature">
+            <h3 className="feature__title">Feature title</h3>
+            <p className="feature__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit illum alias et aliquid consequuntur provident nulla.
+            </p>
+          </div>
+
+          <div className="feature">
+            <h3 className="feature__title">Feature title</h3>
+            <p className="feature__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit illum alias et aliquid consequuntur provident nulla.
+            </p>
+          </div>
+
+          <div className="feature">
+            <h3 className="feature__title">Feature title</h3>
+            <p className="feature__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit illum alias et aliquid consequuntur provident nulla.
+            </p>
+          </div>
+
+          <div className="feature">
+            <h3 className="feature__title">Feature title</h3>
+            <p className="feature__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit illum alias et aliquid consequuntur provident nulla.
+            </p>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,10 @@
+import Features from "../../components/Features/Features";
 import HomePageHero from "../../components/HomePageHero/HomePageHero";
 import "./HomePage.scss";
 
 export default function HomePage() {
   return <>
   <HomePageHero />
+  <Features />
   </>
 }
