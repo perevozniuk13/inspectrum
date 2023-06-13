26fce57547dd2f642ec6f02b767cc48b86e951f9
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-13 19:27:55 +0100

    add get started button to homepage hero, basic styling

--- src/components/HomePageHero/HomePageHero.js
+++ src/components/HomePageHero/HomePageHero.js
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import "./HomePageHero.scss";
 
 export default function HomePageHero() {
+  const navigate = useNavigate();
   return <>
   <section className="homepage-hero">
     <h1 className="homepage-hero__title">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
     {/* <img src="https://via.placeholder.com/300x200" alt="placeholder image" /> */}
+    <button onClick={() => navigate("/login")} className="homepage-hero__get-started-button">get started</button>
   </section>
   </>
 }
