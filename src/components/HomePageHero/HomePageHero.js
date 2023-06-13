540923abc0ac09f3ba1e878ae6787c04ccc6badd
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-13 17:08:04 +0100

    create homepage-hero structure, add basic styling

---
+++ src/components/HomePageHero/HomePageHero.js
@@ -0,0 +1,10 @@
+import "./HomePageHero.scss";
+
+export default function HomePageHero() {
+  return <>
+  <section className="homepage-hero">
+    <h1 className="homepage-hero__title">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+    {/* <img src="https://via.placeholder.com/300x200" alt="placeholder image" /> */}
+  </section>
+  </>
+}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,8 @@
+import HomePageHero from "../../components/HomePageHero/HomePageHero";
 import "./HomePage.scss";
 
 export default function HomePage() {
-  return <>Homepage</>
+  return <>
+  <HomePageHero />
+  </>
 }
