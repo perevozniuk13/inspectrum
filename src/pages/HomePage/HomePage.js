a089bac3cc9854df5a101e14f0585311710189f9
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-28 01:42:05 +0100

    add palettes fan animation

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -12,6 +12,29 @@ export default function HomePage({ isLoggedIn, setIsLoggedIn }) {
     >
       <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <HomePageHero />
+
+      <section className="card-fan">
+        <div className="animation-card1">
+          <div className="animation-card1__color animation-card1__color--1"></div>
+          <div className="animation-card1__color animation-card1__color--2"></div>
+          <div className="animation-card1__color animation-card1__color--3"></div>
+          <div className="animation-card1__color animation-card1__color--4"></div>
+        </div>
+
+        <div className="animation-card2">
+          <div className="animation-card2__color animation-card2__color--1"></div>
+          <div className="animation-card2__color animation-card2__color--2"></div>
+          <div className="animation-card2__color animation-card2__color--3"></div>
+          <div className="animation-card2__color animation-card2__color--4"></div>
+        </div>
+
+        <div className="animation-card3">
+          <div className="animation-card3__color animation-card3__color--1"></div>
+          <div className="animation-card3__color animation-card3__color--2"></div>
+          <div className="animation-card3__color animation-card3__color--3"></div>
+          <div className="animation-card3__color animation-card3__color--4"></div>
+        </div>
+      </section>
       <Features />
       <Footer />
     </section>
