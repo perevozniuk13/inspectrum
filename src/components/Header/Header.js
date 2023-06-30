747771dfb500fbc7628fea8778dc0b7358063885
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-30 12:32:22 +0100

    add mobile styling to header and homepage hero

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import "./Header.scss";
 import { Link, useNavigate } from "react-router-dom";
+import menuIconURL from "../../assets/images/icons8-menu-50.png";
+import { useState } from "react";
 
 export default function Header({ isLoggedIn, setIsLoggedIn }) {
   const navigate = useNavigate();
+  const [menuClicked, setMenuClicked] = useState(false);
 
   const handleLink = (e, to) => {
     e.preventDefault();
@@ -12,6 +15,77 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
 
   return (
     <>
+      <nav className="menu-nav">
+        <Link to="/" className="menu-nav__logo">
+          Inspectrum
+        </Link>
+
+        <img
+          src={menuIconURL}
+          alt="menu icon"
+          className="menu-nav__icon"
+          onClick={() => {
+            if (!menuClicked) {
+              setMenuClicked(true);
+            } else {
+              setMenuClicked(false);
+            }
+          }}
+        />
+        {menuClicked && (
+          <div className="menu-nav__tablet-links-container">
+            <a
+              onClick={(e) => handleLink(e, "/explore")}
+              className="menu-nav__link"
+            >
+              Explore Palettes
+            </a>
+            <a
+              onClick={(e) => handleLink(e, "/create")}
+              className="menu-nav__link"
+            >
+              Create palette
+            </a>
+            <a
+              onClick={(e) => handleLink(e, "/imagePalette")}
+              className="menu-nav__link"
+            >
+              Get Image Palette
+            </a>
+            {!isLoggedIn && (
+              <>
+                <Link to="/login" className="menu-nav__link">
+                  Log In
+                </Link>
+                <Link to="/signup" className="menu-nav__link">
+                  Sign Up
+                </Link>
+              </>
+            )}
+
+            {isLoggedIn && (
+              <>
+                <a
+                  onClick={(e) => handleLink(e, "/profile")}
+                  className="menu-nav__link"
+                >
+                  My Profile
+                </a>
+                <a
+                  onClick={() => {
+                    setIsLoggedIn(false);
+                    sessionStorage.clear();
+                    navigate("/");
+                  }}
+                  className="menu-nav__link"
+                >
+                  Log Out
+                </a>
+              </>
+            )}
+          </div>
+        )}
+      </nav>
       <nav className="nav">
         <Link to="/" className="nav__logo">
           Inspectrum
